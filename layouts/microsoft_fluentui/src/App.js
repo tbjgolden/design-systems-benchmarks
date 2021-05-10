ca5866c7153adaab11c3613d6746e5807bb3c675
Tom Golden <8162045+tbjgolden@users.noreply.github.com>
2021-05-10 18:00:40 -0500

    gave up on fluentui

---
+++ layouts/microsoft_fluentui/src/App.js
@@ -0,0 +1,15 @@
+import { Grid, GridRow, GridCell } from "@fluentui/react"
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Grid>
+        <GridRow>
+          <GridCell>
+            ho
+          </GridCell>
+        </GridRow>
+      </Grid>
+    </div>
+  );
+}
